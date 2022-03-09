c16d698886cf14fad4ee71ce53911724c3a63790
andrew-rosario <71676291+andrew-rosario@users.noreply.github.com>
2022-03-08 19:31:29 -0700

    Add Firestore Firebase

--- app/src/main/java/com/example/qrcodepursuit/MainActivity.java
+++ app/src/main/java/com/example/qrcodepursuit/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.qrcodepursuit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    final String TAG = "Sample";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        dbtest = FirebaseFirestore.getInstance();
+//
+//        final CollectionReference collectionReference = dbtest.collection("Cities");
+//
+//        Map<String, Object> city = new HashMap<>();
+//        city.put("name", "Los Angeles");
+//        city.put("state", "CA");
+//        city.put("country", "USA");
+//
+//        dbtest.collection("cities").document("LA")
+//                .set(city)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Log.d(TAG, "DocumentSnapshot successfully written!");
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error writing document", e);
+//                    }
+//                });
+
+
+
+
     }
 }
