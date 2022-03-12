2cb9301fcad19ab95ab80491de099ba568031797
Kevin Sha <crunchymunchyhoneycakes@gmail.com>
2022-03-12 16:55:04 -0700

    add dependencies and permissions required for QR

---
+++ app/src/main/java/com/team33/qrcodepursuit/CameraActivity.java
@@ -0,0 +1,46 @@
+/*
+MIT License
+
+Copyright (c) 2017 Yuriy Budiyev [yuriy.budiyev@yandex.ru]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+https://github.com/yuriy-budiyev/code-scanner
+
+the above applies to the qr scanner library used
+------------------------------------------------
+the below applies to the qr generator api used
+
+QRGen by kenglxn is licensed under the Apache License, Version 2.0
+https://github.com/kenglxn/QRGen
+ */
+
+package com.team33.qrcodepursuit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
