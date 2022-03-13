4a03822a3e3d4a3508a4befcb0b97944b035f4c3
Kevin Sha <crunchymunchyhoneycakes@gmail.com>
2022-03-12 17:10:44 -0700

    fixes to QR dependencies

--- app/src/main/java/com/team33/qrcodepursuit/CameraActivity.java
+++ app/src/main/java/com/team33/qrcodepursuit/CameraActivity.java
@@ -41,6 +41,6 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camera);
     }
 }
