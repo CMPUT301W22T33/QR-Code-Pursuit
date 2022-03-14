77e433d1d589658c6fc588e9fcedbbf12f8beee6
andrew-rosario <71676291+andrew-rosario@users.noreply.github.com>
2022-03-13 21:27:07 -0600

    Add primitive functions to GameHistory

--- app/src/main/java/com/team33/qrcodepursuit/GameHistory.java
+++ app/src/main/java/com/team33/qrcodepursuit/GameHistory.java
@@ -3,5 +3,27 @@ package com.team33.qrcodepursuit;
 import java.util.ArrayList;
 
 public class GameHistory {
+    ArrayList<GameQRCode> codeHistory;
+    GameStats stats;
+    String associatedUsername;
 
+    public void addCode(GameQRCode gameQRCode){
+        codeHistory.add(gameQRCode);
+    }
+
+    public GameStats getStats(){
+        return this.stats;
+    }
+
+    public void setAssociatedUser(String associatedUsername){
+        this.associatedUsername = associatedUsername;
+    }
+
+    public String getAssociatedUser(){
+        return associatedUsername;
+    }
+
+    public ArrayList<GameQRCode> getCodeHistory(){
+        return this.codeHistory;
+    }
 }
