50d11a22599d83c2613006cc948b8b288825ce99
andrew-rosario <71676291+andrew-rosario@users.noreply.github.com>
2022-03-13 21:29:02 -0600

    Add primitive functions to GameStats

--- app/src/main/java/com/team33/qrcodepursuit/GameStats.java
+++ app/src/main/java/com/team33/qrcodepursuit/GameStats.java
@@ -28,5 +28,55 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class GameStats extends GameHistory {
+    private static final String TAG = "Test";
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+    int totalScore = 0;
+    int totalQRCodes = 0;
+    String associatedUsername;
+    int highestQRAttained;
 
+    GameQRCode currentLowest;
+    GameQRCode currentHighest;
+
+    public void setInitialPointsFromData(){
+        if (totalPoints != 0)
+            GameQRProcessor processor = new GameQRProcessor();
+        ArrayList<GameQRCode> codes = super.codeHistory;
+        int points = 0;
+        for(int i = 0; i < codes.size(); i++){
+            int value = processor.calculateScore(codes[i]);
+            points += value;
+        }
+        setTotalPoints(points);
+    }
+    public void addPoints(int points){
+        this.totalPoints += points;
+    }
+    public void addPoints(GameQRCode code){
+        GameQRProcessor processor = new GameQRProcessor();
+        this.totalPoints += processor.getScore(code);
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public int getTotalQRCodes() {
+        return super.getCodeHistory().size();
+    }
+
+    public GameQRCode getLowestQRCode(){
+        return this.currentLowest;
+    }
+
+    public GameQRCode getHighestQRCode(){
+        return this.currentHighest;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+    public void setTotalQRCodes(int totalQRCodes){
+        this.totalQRCodes = totalQRCodes;
+    }
 }
