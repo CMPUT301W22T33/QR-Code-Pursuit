a324e44d62c5d365463ee8c9ad3f3a94e2172443
Jeff Lee <dlwjdxo98@gmail.com>
2022-03-31 23:04:14 -0600

    add behavior + nav for Login

--- app/src/main/java/com/team33/qrcodepursuit/activities/Login/LoginActivity.java
+++ app/src/main/java/com/team33/qrcodepursuit/activities/Login/LoginActivity.java
@@ -1,115 +1,19 @@
 package com.team33.qrcodepursuit.activities.Login;
 
-import static android.content.ContentValues.TAG;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.team33.qrcodepursuit.activities.MainActivity;
 import com.team33.qrcodepursuit.R;
 
 /**
- * default entry point to app upon launch
  * login to user account using FirebaseAuth
  * if already logged in -> switch to MainActivity
  */
 
 public class LoginActivity extends AppCompatActivity
 {
-    // FirebaseAuth object
-    private FirebaseAuth mAuth;
-    // gui elements
-    private TextInputEditText login_email_field;
-    private TextInputEditText login_passw_field;
-    private Button            login_button;
-    private Button qr_login_button;
-    private TextView          forgotpw_link;
-    private TextView          makeacct_link;
-    private TextView          prvpolicy_link;
-
-    private void gotoMainActivity(FirebaseUser user) {
-        Intent itt = new Intent(LoginActivity.this, MainActivity.class);
-        itt.putExtra("user", user);
-        startActivity(itt);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // Initialize Firebase Auth
-        this.mAuth = FirebaseAuth.getInstance();
-        // link gui elements
-        login_email_field  = findViewById(R.id.login_email_TextInputEditText);
-        login_passw_field  = findViewById(R.id.login_passw_TextInputEditText);
-        login_button = findViewById(R.id.login_button);
-        qr_login_button = findViewById(R.id.qr_login_button);
-        forgotpw_link = findViewById(R.id.forgotpw_link);
-        makeacct_link = findViewById(R.id.makeacct_link);
-        prvpolicy_link = findViewById(R.id.prvpolicy_link);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        // if user already signed in, switch to MainActivity
-        if(currentUser != null)
-            gotoMainActivity(currentUser);
-        // log in with email+pw
-        login_button.setOnClickListener(view -> {
-            Editable email = login_email_field.getText();
-            Editable passw = login_passw_field.getText(); // todo: seems pretty insecure
-            if (email == null || passw == null) return;
-            mAuth.signInWithEmailAndPassword(email.toString(), passw.toString())
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            // switch to MainActivity
-                            Log.d(TAG, "signInWithEmail:success");
-                            gotoMainActivity(mAuth.getCurrentUser());
-                        }
-                        else {
-                            email.clear();
-                            passw.clear();
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        });
-
-        // login with QR
-        qr_login_button.setOnClickListener(view -> {
-            // todo: login with QR fragment
-        });
-
-        // recover password
-        forgotpw_link.setOnClickListener(view -> {
-            // todo: recover password fragment
-        });
-
-        // view privacy policy
-        prvpolicy_link.setOnClickListener(view -> {
-            // todo: link to privacy policy
-        });
-
-        // create new account
-        makeacct_link.setOnClickListener(view -> {
-            // todo: account create fragment
-        });
-
     }
 }
---
+++ app/src/main/java/com/team33/qrcodepursuit/activities/Login/LoginFragment.java
@@ -0,0 +1,99 @@
+package com.team33.qrcodepursuit.activities.Login;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.team33.qrcodepursuit.R;
+import com.team33.qrcodepursuit.activities.MainActivity;
+
+import java.util.concurrent.Executor;
+
+public class LoginFragment extends Fragment{
+
+    // gui elements
+    TextInputEditText login_email_field;
+    TextInputEditText login_passw_field;
+    Button login_button;
+    Button qr_login_button;
+    TextView forgotpw_link;
+    TextView signup_link;
+    TextView prvpolicy_link;
+    NavController navctrl;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        { return inflater.inflate(R.layout.fragment_login, container, false); }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navctrl = Navigation.findNavController(view);
+        // link gui elements
+        login_email_field  = view.findViewById(R.id.login_email_TextInputEditText);
+        login_passw_field  = view.findViewById(R.id.login_passw_TextInputEditText);
+        login_button = view.findViewById(R.id.login_button);
+        qr_login_button = view.findViewById(R.id.login_withqr_button);
+        forgotpw_link = view.findViewById(R.id.login_forgotpw_link);
+        signup_link = view.findViewById(R.id.login_signup_link);
+        prvpolicy_link = view.findViewById(R.id.login_prvpolicy_link);
+        // set button callbacks
+        login_button.setOnClickListener(v -> {
+            // log in with email+pw
+            String email = login_email_field.getText().toString();
+            String passw = login_passw_field.getText().toString();
+            if (email.equals("") || passw.equals("")) return;
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.signInWithEmailAndPassword(email, passw)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // switch to MainActivity
+                            Log.d(TAG, "signInWithEmail:success");
+                            navctrl.navigate(R.id.action_loginFragment_to_mainActivity);
+                        }
+                        else {
+                            Exception e = task.getException();
+                            Log.w(TAG, "signInWithEmail:failure", e);
+                            Toast.makeText(getContext(),
+                                    "Authentication failed:\n" + e.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+        });
+        qr_login_button.setOnClickListener(v ->
+                navctrl.navigate(R.id.action_loginFragment_to_loginWithQRFragment));
+        forgotpw_link.setOnClickListener(v ->
+                navctrl.navigate(R.id.action_loginFragment_to_forgotPwdFragment));
+        prvpolicy_link.setOnClickListener(v ->
+                navctrl.navigate(R.id.action_loginFragment_to_prvPolicyFragment));
+        signup_link.setOnClickListener(v ->
+                navctrl.navigate(R.id.action_loginFragment_to_signupFragment));
+
+        // click outside textedits clears focus
+        view.setOnTouchListener((v,m) -> {
+            v.clearFocus();
+            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            return true;
+        });
+    }
+}
