43b0d8d4bcd4dfc8a27be0667adea95dc21b66c7
Jeff Lee <dlwjdxo98@gmail.com>
2022-04-04 15:51:00 -0600

    basic MyAccountFragment and OtherAccountFragment logic

---
+++ app/src/main/java/com/team33/qrcodepursuit/activities/Account/MyAccountFragment.java
@@ -0,0 +1,46 @@
+package com.team33.qrcodepursuit.activities.Account;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.team33.qrcodepursuit.R;
+import com.team33.qrcodepursuit.models.Account;
+
+public class MyAccountFragment extends AccountFragment {
+
+    public static final String TAG = "MyAccountFragment";
+
+    @Override
+    protected boolean setAccount() {
+        // check login and set account accordingly
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) return false; // not logged in
+        this.acc = new Account(user.getUid(), true);
+        return true;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    protected void settings_button_onclick(View v) {
+        // navigate to settings
+    }
+
+    @Override
+    protected void editpf_button_onclick(View v) {
+        // edit profile (username, contactinfo, bio)
+    }
+
+    @Override
+    protected void collection_button_onclick(View v) {
+        // navigate to my QRcollection
+    }
+}
